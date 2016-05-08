01593c90a53c6d1c9383a465cf9298b581d07c43
Jaymz11 <j.wegrzyn11@gmail.com>
2016-05-08 18:03:19 +0200

    More logic

--- challengeAccepted/www/js/challengelist.js
+++ challengeAccepted/www/js/challengelist.js
@@ -1,23 +1,169 @@
-localStorage['serviceURL'] = "http://coenraets.org/apps/directory/services/";
-var serviceURL = localStorage['serviceURL'];
 
 //var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
-
 var employees;
+var COUNTER_KEY = "COUNTER_KEY";
+var CATEGORY_KEY = "CATEGORY_KEY";
+var CHALLENGE_OBJECT_KEY = "challengeKey";
 
 $(window).load(function() {
-	setTimeout(getEmployeeList, 100);
+	//setTimeout(getEmployeeList, 100);
+	init();
+	//addCategory("Mountain")
+	//addCategory("City")
+	//addCategory("Other")
+	//location.reload();
 });
 
-function getEmployeeList() {
+function init(){
+	if(typeof(Storage) !== "undefined") {
+		var counter = localStorage.getItem(COUNTER_KEY);
+		if(counter == null){
+			counter = 0;
+			localStorage.setItem(COUNTER_KEY, 0);
+		}
+	} else {
+		alert("Sorry! No Web Storage support... App cannot work properly");
+	}
+}
 
-	$('#busy').show();
+function loadBucketList() {
+	$('#list').empty();
+	$('#list').removeData("li");
+	//$('#emptyList').hide();
+
+	counter = localStorage.getItem(COUNTER_KEY);
+	var itemList = new Array();
+	for(i = 0; i < counter; i++){
+		itemList[i] = JSON.parse(localStorage.getItem(CHALLENGE_OBJECT_KEY + (i + 1)));
+	}
 	
-	//Tu tak jak wyzej trzeba ogarnac odczyt/zapis z pamieci. Tylko nie przez php tylko inaczej. JSON moze byc, ale nie musi.
-			$('#list').append('<li><a href="employeedetails.html?id=' + '' + '">' +
-					'<img src="pics/' + 'picture' + '" class="list-icon"/>' +
-					'<p class="line1">' + 'Kuba' + ' ' + 'W' + '</p>' +
-					'<p class="line2">' + 'Mister' + '</p>' +
-					'<span class="bubble">' + 'lolo' + '</span></a></li>');
+	var listContent = new Array();
+	var categories = ["Mountain", "Other", "City"];
+
+	//categories...
+	for (var j = 0; j < itemList.length; j++) {
+		for (var k = 0; k < categories.length; k++) {
+			if(categories[k] === itemList[j].category){
+				
+				if(listContent[k] == null || listContent == undefined){
+					listContent[k] = '<li data-role="list-divider">' +
+					'<h2>' + categories[k] + '</h2>' +
+					'</li>';
+				}
+				listContent[k] += '<li><a href="index.html#pageDetails?id=' + '' + '">' +
+				//'<img src= ' + "img/Matterhorn.jpg" + '>' + 
+				'<h2>' + itemList[j].name + '</h2>' +
+				'</a></li>';
+			}
+		}				
+	}
 	
+	for (var k = 0; k < categories.length; k++) {
+		if(listContent[k] != null && listContent != undefined){
+			$('#list').append(listContent[k]);
+		}
+	}
+	
+	if(itemList.length > 0 ){
+		$('#list').listview('refresh');
+		
+	} else {
+		//$('#emptyList').show();
+	}
+}
+
+function processAddForm(form) {
+	if(validateForm() == true){
+		var item = {
+			name:form.fname.value,
+			localization:form.flocation.value,
+			category:form.fcategory.value
+			//longitude:form.flongitude.value,
+			//lattitude:form.flattitude.value,
+			//img:form.fimage.value,
+			//description:form.fdescription.value
+		};
+
+		store(item);
+	}
+
+    return false;
+}
+
+function validateForm() {
+    var x = document.forms["addForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }	
+	return true;
+}
+
+function store(object) {
+	if(typeof(Storage) !== "undefined") {
+		counter = localStorage.getItem(COUNTER_KEY);
+		stringified = JSON.stringify(object);
+		
+		localStorage.setItem(CHALLENGE_OBJECT_KEY + String(++counter), stringified);
+		localStorage.setItem(COUNTER_KEY, counter);
+
+		alert("Successfully stored new Bucket Challenge");
+		return true;
+	
+	} else {
+		alert("Sorry! No Web Storage support..");
+	}
+	return false;
+}
+
+function clearLocalStorage() {
+	localStorage.clear();
+	init();
+	alert("Successfully cleared the list");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Not used. Yet?  ============================================
+function getCategories(){
+	jsonCategories = localStorage.getItem(CATEGORY_KEY);
+	if(jsonCategories == null){
+		return null;
+	}
+	
+	return JSON.parse(jsonCategories);
+}
+
+function addCategory(category){
+	categories = getCategories();
+
+	var arrayLength = categories.length;
+	for (var i = 0; i < arrayLength; i++) {
+		if(categories[i] === category){
+			alert(category + " is already defined.");
+			return false;
+		}
+	}
+	categories[arrayLength] = category;
+	storeCategory(categories);
+	return true;
+}
+
+function storeCategory(categories){
+	jsonCategories = localStorage.setItem(CATEGORY_KEY, JSON.stringify(categories));
 }
